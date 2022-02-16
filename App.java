6bbc8169df6c6ad4e36bc8cf479bcd090118a397
Megyeri <megyko1234@gmail.com>
2022-02-16 09:58:34 +0100

    Kezdes

---
+++ App.java
@@ -0,0 +1,25 @@
+
+/*
+* File: App.java
+* Author: Megyeri Mark	
+* Copyright: 2022, Megyeri Mark
+* Group: Szoft 1/N
+* Date: 2022-02-16
+* Github: https://github.com/megyerimark/
+* Licenc: GNU GPL
+*/
+
+
+
+import java.util.Scanner;
+
+class App {
+	public static void main (String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Megyeri Mark , 2022-02-16");
+		System.out.println("Feladat 0306");
+		System.out.println("Haromszog teruletszamitas");
+		}
+	
+	}
